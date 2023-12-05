1b29663558dfc555e3f6571a6cc7b31872868c46
alanhakhyeonsong <songs4805@naver.com>
2023-12-05 23:30:44 +0900

    Chapter 5 ref + 내부 메서드

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
-import ValidationSample from './chapter05/ValidationSample';
+import React, { Component } from 'react';
+import ScrollBox from './chapter05/ScrollBox';
 
-function App() {
-  return <ValidationSample />
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
