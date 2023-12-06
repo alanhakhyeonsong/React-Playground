787694d0e6b35be372ab52479f772d6b95d653f7
alanhakhyeonsong <songs4805@naver.com>
2023-12-06 16:29:29 +0900

    key prop이 필요하다. 다만 배열의 index 사용은 지양하자.

--- playground/src/chapter06/IterationSample.js
+++ playground/src/chapter06/IterationSample.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IterationSample = () => {
   const values = ['1', '2', '3', '4'];
-  const valueList = values.map(val => <li>{val}</li>);
+  const valueList = values.map((val, index) => <li key={index}>{val}</li>);
 
   return (
     <ul>{valueList}</ul>
