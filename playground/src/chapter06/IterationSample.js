7abba16c84cce59cec399be7394345728ded81fb
alanhakhyeonsong <songs4805@naver.com>
2023-12-06 16:41:27 +0900

    data input 추가

--- playground/src/chapter06/IterationSample.js
+++ playground/src/chapter06/IterationSample.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-  const values = ['1', '2', '3', '4'];
-  const valueList = values.map((val, index) => <li key={index}>{val}</li>);
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' }
+  ]);
+
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+  const onChange = e => setInputText(e.target.value);
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, // nextId 값을 id로 설정
+      text: inputText
+    });
+    setNextId(nextId + 1); // nextId 값에 1을 더한다.
+    setNames(nextNames); // names 값 업데이트
+    setInputText('');
+  }
+
+  const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
 
   return (
-    <ul>{valueList}</ul>
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>
+    </>
   );
 };
 
