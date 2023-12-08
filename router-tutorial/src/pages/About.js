54386bae2d696e21ab02d4f522ce298610665fcd
alanhakhyeonsong <songs4805@naver.com>
2023-12-09 00:27:44 +0900

    React Router 실습 - queryParameter

--- router-tutorial/src/pages/About.js
+++ router-tutorial/src/pages/About.js
@@ -1,8 +1,27 @@
+import { useSearchParams } from 'react-router-dom';
+
 const About = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get('detail');
+  const mode = searchParams.get('mod');
+
+  const onToggleDetail = () => {
+    setSearchParams({ mode, detail: detail === 'true' ? false : true });
+  }
+
+  const onIncreaseMode = () => {
+    const nextMode = mode === null ? 1 : parseInt(mode) + 1;
+    setSearchParams({ mode: nextMode, detail });
+  };
+
   return (
     <div>
       <h1>About</h1>
       <p>Ramos의 리액트 라우터 뽀개기...</p>
+      <p>detail: {detail}</p>
+      <p>mode: {mode}</p>
+      <button onClick={onToggleDetail}>Toggle detail</button>
+      <button onClick={onIncreaseMode}>mode + 1</button>
     </div>
   );
 };
