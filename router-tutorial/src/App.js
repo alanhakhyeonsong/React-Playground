03065174af2c60b867462455584f06a7796d51f6
alanhakhyeonsong <songs4805@naver.com>
2023-12-08 23:44:01 +0900

    React Router 실습

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+};
+
+export default App;
