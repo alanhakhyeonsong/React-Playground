4bbd80437eca71bbded7c23384c7536008e51366
alanhakhyeonsong <songs4805@naver.com>
2023-12-17 00:42:45 +0900

    init recoil-tutorial

---
+++ recoil-tutorial/src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>hi</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
